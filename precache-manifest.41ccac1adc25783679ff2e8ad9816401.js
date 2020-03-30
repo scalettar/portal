0ddc80b4b7d5c084e73bac7fd970e2e19baac76f
Daniel Scalettar <scalettar+github@gmail.com>
2020-03-29 18:45:21 -0700

    Updates

---
+++ precache-manifest.41ccac1adc25783679ff2e8ad9816401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966de8d18be92c73a1312e630c4a47bd",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b0d868cbbb9178e0567a",
+    "url": "/portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f3021b7ef5dcfbcdb3e0",
+    "url": "/portal/static/js/2.1b79a414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portal/static/js/2.1b79a414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d868cbbb9178e0567a",
+    "url": "/portal/static/js/main.5b141807.chunk.js"
+  },
+  {
+    "revision": "1421a0e2c121c71f9b09",
+    "url": "/portal/static/js/runtime-main.8778c6d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
