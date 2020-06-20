faef6dc72d8893d1490457ece63e504f8e0f0c29
Daniel Scalettar <git@scalettar.com>
2020-06-20 11:57:20 -0700

    Updates

--- precache-manifest.e6789ceb38eb7f14b0f5fc7db7d0f81f.js
+++ precache-manifest.e6789ceb38eb7f14b0f5fc7db7d0f81f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "686f30e2d9d97275515633298a057210",
+    "revision": "e7d05096d8805f443003d4560e0ac6d4",
     "url": "/index.html"
   },
   {
-    "revision": "5aa5f5226fe4b59d606d",
-    "url": "/static/css/main.2e15abf1.chunk.css"
+    "revision": "58ea7e954c8a9c2541ca",
+    "url": "/static/css/main.ab656f95.chunk.css"
   },
   {
     "revision": "81af944354d44d82cc42",
@@ -16,15 +16,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.23a073db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5aa5f5226fe4b59d606d",
-    "url": "/static/js/main.6f8524d8.chunk.js"
+    "revision": "58ea7e954c8a9c2541ca",
+    "url": "/static/js/main.48520504.chunk.js"
   },
   {
     "revision": "f93384a7d00fd3037048",
     "url": "/static/js/runtime-main.b87878cf.js"
-  },
-  {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
   }
 ]);
